55efde938b616d1de34800da4ba953d71349a27c
Erdbeergeist <luxmitges@gmail.com>
2015-03-31 12:14:14 +0200

    changes to jobfile

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #BSUB -L /bin/bash
-#BSUB -o /home/lmittnac/HadRec/output/out.out
-#BSUB -e /home/lmittnac/HadRec/output/errout.out
+#BSUB -o /home/lmittnac/HadronicRecoil/output/out.out
+#BSUB -e /home/lmittnac/HadronicRecoil/output/errout.out
 #BSUB -q atlaslong
 #BSUB -app Reserve3G
 #BSUB -n 3
@@ -11,4 +11,4 @@ source /cluster/cern/root_v5.34.03_x86_64/bin/thisroot.sh
 
 make
 
-/home/lmittnac/HadRec/hardrec /etapfs02/atlashpc/stilch/Preparation/Results_merged.root /home/lmittnac/HadRec/out.root
+/home/lmittnac/HadronicRecoil/hadrec /etapfs02/atlashpc/stilch/Preparation/Results_merged.root /home/lmittnac/HadronicRecoil/out.root
