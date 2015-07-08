5fb7e6843a0973cdaf83dc9923a26e2089a41296
Erdbeergeist <luxmitges@web.de>
2015-07-08 18:35:53 +0200

    new hist name

--- NoAssoHadrec/hadrec_functions.h
+++ NoAssoHadrec/hadrec_functions.h
@@ -54,7 +54,7 @@ double GetMinBiasMean(TFile *fileAI,double pt, double eta, int NumberofVertices)
 	//cout<<sortpt<<"\t"<<sorteta<<"\t"<<NumberofVertices<<endl;	
 	string  name = "SumPTEta"+to_string(static_cast<long long int>(sorteta)) +"NV"+ to_string(static_cast<long long int>(NumberofVertices)) + "PT"+to_string(static_cast<long long int>(sortpt));
 	//cout<<name<<endl;
-	TH1F * h1 = new TH1F("h1","h1 title" , 100, 0, 4);
+	TH1F * h1 = new TH1F(name.c_str(),name.c_str() , 100, 0, 4);
 	h1 = (TH1F*)fileAI->Get(name.c_str());
 	mean = 100;//h1->GetMean();
 	
